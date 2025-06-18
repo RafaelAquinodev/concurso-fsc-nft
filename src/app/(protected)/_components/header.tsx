29212f61336ad5462cf423ad7a7b2fc90c3e8270
Erick Araújo <araujoerick.dev@gmail.com>
2025-06-18 02:30:29 -0300

    (feat): add wallet context to ensure wallet data can be used in all pages

--- src/app/(protected)/_components/header.tsx
+++ src/app/(protected)/_components/header.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import {
   Select,
@@ -8,17 +10,21 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { walletCatalog } from "@/data/wallet-catalog";
 import { Bell } from "lucide-react";
 import {
   Tooltip,
   TooltipContent,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import { useWallet } from "@/context/wallet-context";
 
 const Header = () => {
+  const { walletAddress, setWalletAddress } = useWallet();
+
   return (
     <header className="flex w-full items-center justify-between border-b p-4">
-      <Select>
+      <Select value={walletAddress} onValueChange={setWalletAddress}>
         <SelectTrigger className="w-[280px]">
           <SelectValue placeholder="Selecione uma carteira" />
         </SelectTrigger>
--- src/app/(protected)/nfts/_components/nft-cards.tsx
+++ src/app/(protected)/nfts/_components/nft-cards.tsx
@@ -3,13 +3,10 @@
 import { useWalletNFTs } from "@/hooks/use-wallet-nfts";
 import React from "react";
 import NftCard from "./nft-card";
+import { useWallet } from "@/context/wallet-context";
 
 const NftCards = () => {
-  const [walletAddress, setWalletAddress] = React.useState(
-    // "0xb7f7f6c52f2e2fdb1963eab30438024864c313f6", // Maior carteira de CryptoPunks
-    "0xe2a83b15fc300d8457eb9e176f98d92a8ff40a49", // Maior carteira de Bored Apes
-    // "0xc4505db8cc490767fa6f4b6f0f2bdd668b357a5d" // Neymar Jr. Wallet
-  );
+  const { walletAddress } = useWallet();
 
   const { nfts, loading, error, hasNextPage, refetch, loadMore } =
     useWalletNFTs({
