784be8b666009598e5fd6acfe03e834266f42511
Erick Araújo <araujoerick.dev@gmail.com>
2025-06-23 12:50:35 -0300

    (feat): remove select from favorites and upgrade pages

--- src/app/(protected)/_components/header.tsx
+++ src/app/(protected)/_components/header.tsx
@@ -22,8 +22,11 @@ import {
 import { useWallet } from "@/context/wallet-context";
 import { AddWalletModal } from "./add-wallet-modal";
 import { Button } from "@/components/ui/button";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
+  const pathname = usePathname();
+
   const { walletAddress, setWalletAddress, allWallets } = useWallet();
 
   const defaultWallets = allWallets.filter((wallet) =>
@@ -40,78 +43,84 @@ const Header = () => {
 
   return (
     <header className="flex w-full items-center justify-between gap-2 border-b p-4">
-      <div className="flex items-center gap-2">
-        <Select value={walletAddress} onValueChange={setWalletAddress}>
-          <SelectTrigger className="w-[280px]">
-            <SelectValue placeholder="Selecione uma carteira" />
-          </SelectTrigger>
-          <SelectContent>
-            {/* Carteiras customizadas */}
-            {customWallets.length > 0 && (
-              <SelectGroup>
-                <SelectLabel>Minhas Carteiras</SelectLabel>
-                {customWallets.map((wallet) => (
-                  <Tooltip key={wallet.address}>
-                    <TooltipTrigger asChild>
-                      <SelectItem value={wallet.address}>
-                        <div className="flex w-full items-center justify-between">
-                          <span>{wallet.name}</span>
-                        </div>
-                      </SelectItem>
-                    </TooltipTrigger>
-                    <TooltipContent side="right">
-                      <div className="space-y-1">
-                        <div className="text-sm text-gray-500">
-                          {wallet.description}
-                        </div>
-                        <div className="hidden font-mono text-xs text-gray-400 md:block">
-                          {wallet.address}
-                        </div>
-                      </div>
-                    </TooltipContent>
-                  </Tooltip>
-                ))}
-              </SelectGroup>
-            )}
+      <div>
+        {!["/favorites", "/upgrade"].includes(pathname) && (
+          <div className="flex items-center gap-2">
+            <Select value={walletAddress} onValueChange={setWalletAddress}>
+              <SelectTrigger className="w-[280px]">
+                <SelectValue placeholder="Selecione uma carteira" />
+              </SelectTrigger>
+              <SelectContent>
+                {/* Carteiras customizadas */}
+                {customWallets.length > 0 && (
+                  <SelectGroup>
+                    <SelectLabel>Minhas Carteiras</SelectLabel>
+                    {customWallets.map((wallet) => (
+                      <Tooltip key={wallet.address}>
+                        <TooltipTrigger asChild>
+                          <SelectItem value={wallet.address}>
+                            <div className="flex w-full items-center justify-between">
+                              <span>{wallet.name}</span>
+                            </div>
+                          </SelectItem>
+                        </TooltipTrigger>
+                        <TooltipContent side="right">
+                          <div className="space-y-1">
+                            <div className="text-sm text-gray-500">
+                              {wallet.description}
+                            </div>
+                            <div className="hidden font-mono text-xs text-gray-400 md:block">
+                              {wallet.address}
+                            </div>
+                          </div>
+                        </TooltipContent>
+                      </Tooltip>
+                    ))}
+                  </SelectGroup>
+                )}
 
-            {/* Carteiras padrão */}
-            <SelectGroup>
-              <SelectLabel>Carteiras Padrão</SelectLabel>
-              {defaultWallets.map((wallet) => (
-                <Tooltip key={wallet.address}>
-                  <TooltipTrigger asChild>
-                    <SelectItem value={wallet.address}>
-                      {wallet.name}
-                    </SelectItem>
-                  </TooltipTrigger>
-                  <TooltipContent side="right">
-                    <div className="space-y-1">
-                      <div className="text-sm text-gray-500">
-                        {wallet.description}
-                      </div>
-                      <div className="hidden font-mono text-xs text-gray-400 md:block">
-                        {wallet.address}
-                      </div>
-                    </div>
-                  </TooltipContent>
-                </Tooltip>
-              ))}
-            </SelectGroup>
-          </SelectContent>
-        </Select>
+                {/* Carteiras padrão */}
+                <SelectGroup>
+                  <SelectLabel>Carteiras Padrão</SelectLabel>
+                  {defaultWallets.map((wallet) => (
+                    <Tooltip key={wallet.address}>
+                      <TooltipTrigger asChild>
+                        <SelectItem value={wallet.address}>
+                          {wallet.name}
+                        </SelectItem>
+                      </TooltipTrigger>
+                      <TooltipContent side="right">
+                        <div className="space-y-1">
+                          <div className="text-sm text-gray-500">
+                            {wallet.description}
+                          </div>
+                          <div className="hidden font-mono text-xs text-gray-400 md:block">
+                            {wallet.address}
+                          </div>
+                        </div>
+                      </TooltipContent>
+                    </Tooltip>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
 
-        <Tooltip>
-          <AddWalletModal>
-            <TooltipTrigger asChild>
-              <Button className="flex items-center gap-2 rounded-lg bg-purple-600 px-2 py-1 text-white transition-colors hover:bg-purple-700">
-                <PlusIcon width={20} />
-              </Button>
-            </TooltipTrigger>
-          </AddWalletModal>
-          <TooltipContent side="right">
-            <div className="text-sm text-gray-500">Adicionar nova carteira</div>
-          </TooltipContent>
-        </Tooltip>
+            <Tooltip>
+              <AddWalletModal>
+                <TooltipTrigger asChild>
+                  <Button className="flex items-center gap-2 rounded-lg bg-purple-600 px-2 py-1 text-white transition-colors hover:bg-purple-700">
+                    <PlusIcon width={20} />
+                  </Button>
+                </TooltipTrigger>
+              </AddWalletModal>
+              <TooltipContent side="right">
+                <div className="text-sm text-gray-500">
+                  Adicionar nova carteira
+                </div>
+              </TooltipContent>
+            </Tooltip>
+          </div>
+        )}
       </div>
       <Avatar>
         <UserButton />
