4aa12b3f65ba7c35276e5e777f1a5735190f74a4
Erick Araújo <araujoerick.dev@gmail.com>
2025-06-30 17:31:04 -0300

    (feat): update sidebar and header colors

--- src/app/(protected)/_components/header.tsx
+++ src/app/(protected)/_components/header.tsx
@@ -43,7 +43,7 @@ const Header = () => {
   );
 
   return (
-    <header className="flex w-full items-center justify-between gap-2 border-b p-4">
+    <header className="bg-brand-indigo flex w-full items-center justify-between gap-2 border-b p-4">
       <div className="flex items-center gap-3">
         <SidebarTrigger />
 
