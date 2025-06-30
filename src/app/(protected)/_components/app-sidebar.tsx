0cb2eace3a115b8e81ef61232ff4afa29817f4ab
Erick Araújo <araujoerick.dev@gmail.com>
2025-06-30 17:30:07 -0300

    (feat): update sidebar buttons, colors and size style and add upgrade card on footer

--- src/app/(protected)/_components/app-sidebar.tsx
+++ src/app/(protected)/_components/app-sidebar.tsx
@@ -3,9 +3,9 @@
 import {
   ArrowRightLeft,
   LayoutDashboard,
+  LockIcon,
   Palette,
   StarIcon,
-  Trophy,
 } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
@@ -14,15 +14,22 @@ import { usePathname } from "next/navigation";
 import {
   Sidebar,
   SidebarContent,
+  SidebarFooter,
   SidebarGroup,
   SidebarGroupContent,
   SidebarHeader,
   SidebarMenu,
+  SidebarMenuBadge,
   SidebarMenuButton,
   SidebarMenuItem,
   useSidebar,
 } from "@/components/ui/sidebar";
 import { useUser } from "@clerk/nextjs";
+import {
+  TooltipTrigger,
+  TooltipContent,
+  Tooltip,
+} from "@/components/ui/tooltip";
 
 const items = [
   {
@@ -51,11 +58,11 @@ export function AppSidebar() {
   return (
     <Sidebar
       collapsible="icon"
-      className="group w-64 transition-all duration-300 group-data-[collapsible=icon]:w-14"
+      className="group w-64 border-[#34323f] transition-all duration-300 group-data-[collapsible=icon]:w-14"
     >
       <SidebarHeader className="flex min-h-[70px] items-center justify-center">
         {open ? (
-          <div className="flex items-center gap-4">
+          <div className="ml-1 flex items-center gap-2 self-start">
             <Image src="/logo.svg" alt="Logo" width={32} height={32} />
             <h2 className="gradient-underline text-xl font-semibold">
               NFTMetrics
@@ -77,7 +84,7 @@ export function AppSidebar() {
                     size="lg"
                     asChild
                     isActive={pathname === item.url}
-                    className="group-data-[collapsible=icon]:justify-center group-data-[collapsible=icon]:[&>span]:hidden [&>svg]:h-6 [&>svg]:w-6"
+                    className="group-data-[collapsible=icon]:justify-center group-data-[collapsible=icon]:[&>span]:hidden [&>svg]:h-5 [&>svg]:w-5"
                   >
                     <Link href={item.url}>
                       <item.icon />
@@ -87,13 +94,13 @@ export function AppSidebar() {
                 </SidebarMenuItem>
               ))}
             </SidebarMenu>
-            {premiumPlan ? (
+            {!premiumPlan ? (
               <SidebarMenu className="mt-2 group-data-[collapsible=icon]:mt-6 group-data-[collapsible=icon]:gap-6">
                 <SidebarMenuButton
                   size="lg"
                   asChild
                   isActive={pathname === "/favorites"}
-                  className="group-data-[collapsible=icon]:justify-center group-data-[collapsible=icon]:[&>span]:hidden [&>svg]:h-6 [&>svg]:w-6"
+                  className="group-data-[collapsible=icon]:justify-center group-data-[collapsible=icon]:[&>span]:hidden [&>svg]:h-5 [&>svg]:w-5"
                 >
                   <Link href="/favorites">
                     <StarIcon />
@@ -103,36 +110,57 @@ export function AppSidebar() {
               </SidebarMenu>
             ) : (
               <SidebarMenu className="mt-2 group-data-[collapsible=icon]:mt-6 group-data-[collapsible=icon]:gap-6">
-                <SidebarMenuButton
-                  size="lg"
-                  asChild
-                  isActive={pathname === "/favorites"}
-                  className="group-data-[collapsible=icon]:justify-center group-data-[collapsible=icon]:[&>span]:hidden [&>svg]:h-6 [&>svg]:w-6"
-                >
-                  <span className="opacity-5">
-                    <StarIcon />
-                    <span>Favoritos</span>
-                  </span>
-                </SidebarMenuButton>
+                <Tooltip>
+                  <TooltipTrigger>
+                    <SidebarMenuItem>
+                      <SidebarMenuButton
+                        size="lg"
+                        asChild
+                        isActive={pathname === "/favorites"}
+                        className="group-data-[collapsible=icon]:justify-center group-data-[collapsible=icon]:[&>span]:hidden [&>svg]:h-5 [&>svg]:w-5"
+                      >
+                        <div className="opacity-50 transition-opacity duration-200">
+                          <StarIcon />
+                          <span className="text-lg">Favoritos</span>
+                          <SidebarMenuBadge className="">
+                            <LockIcon size={12} className="mr-4" />
+                          </SidebarMenuBadge>
+                        </div>
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                  </TooltipTrigger>
+                  <TooltipContent side="right">
+                    <p className="">Disponível apenas no plano Premium</p>
+                  </TooltipContent>
+                </Tooltip>
               </SidebarMenu>
             )}
           </SidebarGroupContent>
         </SidebarGroup>
-        <SidebarGroup>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              <SidebarMenuItem>
-                <SidebarMenuButton asChild isActive={pathname === "/upgrade"}>
-                  <Link href="/upgrade">
-                    <Trophy color="#878036" />
-                    <span>Upgrade</span>
-                  </Link>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
       </SidebarContent>
+      {premiumPlan && (
+        <SidebarFooter>
+          {open ? (
+            <div className="mt-auto px-4 py-6">
+              <div className="bg-secondary rounded-xl border border-gray-600 p-4">
+                <h3 className="text-sm font-medium text-white">
+                  Upgrade para Premium
+                </h3>
+                <p className="mt-1 text-xs text-gray-300">
+                  Monitore carteiras ilimitadas e salve NFTs como favoritos.
+                </p>
+                <Link href="/upgrade">
+                  <button className="gradient-brand mt-3 w-full cursor-pointer rounded-lg px-4 py-2 text-sm font-semibold text-white transition duration-200 hover:shadow-[0_0_8px_rgba(255,0,204,0.6)]">
+                    Upgrade Now
+                  </button>
+                </Link>
+              </div>
+            </div>
+          ) : (
+            <div></div>
+          )}
+        </SidebarFooter>
+      )}
     </Sidebar>
   );
 }
