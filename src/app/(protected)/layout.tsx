769a9e097f25692689df88bb3ae6065201fc90c9
Erick Araújo <araujoerick.dev@gmail.com>
2025-06-30 04:06:21 -0300

    (feat): change sidebar and header position on dom

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,10 +1,11 @@
 "use client";
+
 import { useUser } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import Header from "./_components/header";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "./_components/app-sidebar";
-import Header from "./_components/header";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const { isSignedIn, isLoaded } = useUser();
@@ -21,21 +22,12 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   }
 
   return (
-    <SidebarProvider open={true} className="overflow-hidden">
+    <SidebarProvider defaultOpen={true}>
       <AppSidebar />
-
-      <div className="ml-[240px] w-full">
-        <HeaderWrapper />
-        <main className="pt-[64px]">{children}</main>
+      <div className="w-full">
+        <Header />
+        <main className="">{children}</main>
       </div>
     </SidebarProvider>
-  );
-}
-
-function HeaderWrapper() {
-  return (
-    <div className="bg-background fixed top-0 right-0 left-[255px] z-40 border-l">
-      <Header />
-    </div>
   );
 }
