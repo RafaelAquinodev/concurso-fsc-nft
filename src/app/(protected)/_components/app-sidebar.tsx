d2f5557802e3519fb1da3e7aad5035722ac69c68
Erick Araújo <araujoerick.dev@gmail.com>
2025-06-30 04:10:55 -0300

    (feat): change menu buttons size and style

--- src/app/(protected)/_components/app-sidebar.tsx
+++ src/app/(protected)/_components/app-sidebar.tsx
@@ -2,9 +2,9 @@
 
 import {
   ArrowRightLeft,
-  Heart,
   LayoutDashboard,
   Palette,
+  StarIcon,
   Trophy,
 } from "lucide-react";
 import Image from "next/image";
@@ -20,8 +20,8 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
+  useSidebar,
 } from "@/components/ui/sidebar";
-import { Separator } from "@/components/ui/separator";
 import { useUser } from "@clerk/nextjs";
 
 const items = [
@@ -40,57 +40,84 @@ const items = [
     url: "/transactions",
     icon: ArrowRightLeft,
   },
-  // {
-  //   title: "Favoritos",
-  //   url: "/favorites",
-  //   icon: Heart,
-  // },
 ];
 
 export function AppSidebar() {
   const pathname = usePathname();
   const { user } = useUser();
   const premiumPlan = user?.publicMetadata.subscriptionPlan === "premium";
+  const { open } = useSidebar();
 
   return (
-    <Sidebar className="bg-sidebar fixed h-screen border-r" collapsible="none">
-      <SidebarHeader className="flex justify-center p-4">
-        <Image src="/logo.svg" alt="FSC" width={40} height={40} />
+    <Sidebar
+      collapsible="icon"
+      className="group w-64 transition-all duration-300 group-data-[collapsible=icon]:w-14"
+    >
+      <SidebarHeader className="flex min-h-[70px] items-center justify-center">
+        {open ? (
+          <div className="flex items-center gap-4">
+            <Image src="/logo.svg" alt="Logo" width={32} height={32} />
+            <h2 className="gradient-underline text-xl font-semibold">
+              NFTMetrics
+            </h2>
+          </div>
+        ) : (
+          <div className="flex flex-col items-center gap-4">
+            <Image src="/logo.svg" alt="Logo" width={22} height={22} />
+          </div>
+        )}
       </SidebarHeader>
-      <SidebarContent>
+      <SidebarContent className="group-data-[collapsible=icon]:mx-auto group-data-[collapsible=icon]:items-center">
         <SidebarGroup>
           <SidebarGroupContent>
-            <SidebarMenu>
+            <SidebarMenu className="gap-2 group-data-[collapsible=icon]:mt-2 group-data-[collapsible=icon]:gap-6">
               {items.map((item) => (
                 <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild isActive={pathname === item.url}>
+                  <SidebarMenuButton
+                    size="lg"
+                    asChild
+                    isActive={pathname === item.url}
+                    className="group-data-[collapsible=icon]:justify-center group-data-[collapsible=icon]:[&>span]:hidden [&>svg]:h-6 [&>svg]:w-6"
+                  >
                     <Link href={item.url}>
                       <item.icon />
-                      <span>{item.title}</span>
+                      <span className="text-lg">{item.title}</span>
                     </Link>
                   </SidebarMenuButton>
                 </SidebarMenuItem>
               ))}
             </SidebarMenu>
             {premiumPlan ? (
-              <SidebarMenuButton asChild isActive={pathname === "/favorites"}>
-                <Link href="/favorites">
-                  <Heart />
-                  <span>Favoritos</span>
-                </Link>
-              </SidebarMenuButton>
+              <SidebarMenu className="mt-2 group-data-[collapsible=icon]:mt-6 group-data-[collapsible=icon]:gap-6">
+                <SidebarMenuButton
+                  size="lg"
+                  asChild
+                  isActive={pathname === "/favorites"}
+                  className="group-data-[collapsible=icon]:justify-center group-data-[collapsible=icon]:[&>span]:hidden [&>svg]:h-6 [&>svg]:w-6"
+                >
+                  <Link href="/favorites">
+                    <StarIcon />
+                    <span className="text-lg">Favoritos</span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenu>
             ) : (
-              <SidebarMenuButton asChild isActive={pathname === "/favorites"}>
-                {/* Aqui, trocar Link por span para desabilitar o clique */}
-                <span style={{ cursor: "not-allowed", opacity: 0.5 }}>
-                  <Heart />
-                  <span>Favoritos</span>
-                </span>
-              </SidebarMenuButton>
+              <SidebarMenu className="mt-2 group-data-[collapsible=icon]:mt-6 group-data-[collapsible=icon]:gap-6">
+                <SidebarMenuButton
+                  size="lg"
+                  asChild
+                  isActive={pathname === "/favorites"}
+                  className="group-data-[collapsible=icon]:justify-center group-data-[collapsible=icon]:[&>span]:hidden [&>svg]:h-6 [&>svg]:w-6"
+                >
+                  <span className="opacity-5">
+                    <StarIcon />
+                    <span>Favoritos</span>
+                  </span>
+                </SidebarMenuButton>
+              </SidebarMenu>
             )}
           </SidebarGroupContent>
         </SidebarGroup>
-        <Separator />
         <SidebarGroup>
           <SidebarGroupContent>
             <SidebarMenu>
