fcebfe3f11832153181792dcfd3eb7fc2c187bc6
Erick Araújo <araujoerick.dev@gmail.com>
2025-07-05 16:25:54 -0300

    (reafctor): ensure user do not access favorites page when not premium

--- src/app/(protected)/favorites/page.tsx
+++ src/app/(protected)/favorites/page.tsx
@@ -1,7 +1,20 @@
+import { currentUser } from "@clerk/nextjs/server";
 import { PageContainer } from "../_components/page-container";
 import FavoriteNftCards from "./_components/favorite-cards";
+import { redirect } from "next/navigation";
 
-const FavoritesPage = () => {
+const FavoritesPage = async () => {
+  const user = await currentUser();
+
+  if (!user) {
+    return redirect("/");
+  }
+  const isPremium = user.publicMetadata?.subscriptionPlan === "premium";
+
+  if (!isPremium) {
+    return redirect("/upgrade");
+  }
+
   return (
     <PageContainer>
       <div className="flex flex-wrap gap-8">
--- src/app/(protected)/upgrade/page.tsx
+++ src/app/(protected)/upgrade/page.tsx
@@ -1,11 +1,11 @@
 import { PageContainer } from "../_components/page-container";
 import UpgradePlan from "./_components/upgrade-card";
 
-const NftsPage = () => {
+const UpgradePage = () => {
   return (
     <PageContainer>
       <UpgradePlan />
     </PageContainer>
   );
 };
-export default NftsPage;
+export default UpgradePage;
