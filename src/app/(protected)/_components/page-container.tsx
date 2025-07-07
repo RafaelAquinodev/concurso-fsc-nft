c85618b4f28cebfba2212943013a9a876aee9027
Erick Araújo <araujoerick.dev@gmail.com>
2025-07-07 14:38:19 -0300

    (refactor): update the base layout style

--- src/app/(protected)/_components/page-container.tsx
+++ src/app/(protected)/_components/page-container.tsx
@@ -1,5 +1,5 @@
 export const PageContainer = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="bg-secondary h-full w-full py-6 px-8">{children}</div>
+    <div className="bg-secondary h-full w-full p-4 sm:p-8">{children}</div>
   );
 };
